9007a9ca47e25257c6cb9ea07bbb270aae7dd4c3
marshmallowforme <104122985+marshmallowforme@users.noreply.github.com>
2025-03-26 15:31:31 +0000

    mod serverjs

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 //Midleware
 app.use(express.json());
 app.use(require("cors")());
+
+console.log("MongoDB URI:", process.env.MONGO_URI);
 
 //connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {useNewUrlPraser: true, useUnifiedTopology: true,
