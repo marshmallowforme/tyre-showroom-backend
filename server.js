87705ec5892662e875d02504c461c251e56993c0
marshmallowforme <104122985+marshmallowforme@users.noreply.github.com>
2025-03-26 15:36:06 +0000

    addedd reuire txt

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(require("cors")());
 
-console.log("MongoDB URI:", process.env.MONGO_URI);
+
 
 //connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {useNewUrlPraser: true, useUnifiedTopology: true,
